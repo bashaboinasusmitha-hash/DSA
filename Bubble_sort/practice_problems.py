b99d78de72d0d74c0effb6005afe9064791a035b
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-07-24 19:13:25 +0530

    added a program of improvement of bubble sort

--- Bubble_sort/practice_problems.py
+++ Bubble_sort/practice_problems.py
@@ -14,3 +14,25 @@ for i in range(n-1):
         if nums[j]<nums[j+1]:
             nums[j],nums[j+1]=nums[j+1],nums[j]
 print(nums)
+#count the number of swaps :
+nums=[8, 3, 1, 6, 4]
+n=len(nums)
+res=0
+for i in range(n-1):
+    for j in range(n-i-1):
+        if nums[j]>nums[j+1]:
+            nums[j],nums[j+1]=nums[j+1],nums[j]
+            res+=1
+print(res)
+#improvement of bubble sort:
+nums=[3,4,1,2,7,0]
+n=len(nums)
+for i in range(n-1):
+    swap=False
+    for j in range(n-i-1):
+        if nums[j]>nums[j+1]:
+            nums[j],nums[j+1]=nums[j+1],nums[j]
+            swap=True
+    if not swap:
+        break   
+print(nums)
