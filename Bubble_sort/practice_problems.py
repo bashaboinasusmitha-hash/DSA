f0972c6a89a31c6cdc3c0f4344750ade4ea39811
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-07-26 17:00:52 +0530

    added a practice prblms on bubble sort

--- Bubble_sort/practice_problems.py
+++ Bubble_sort/practice_problems.py
@@ -62,3 +62,38 @@ for i in range(n-1):
     print(f"{i+1} : {nums}")
     if not swap:
         break
+#sort only even numbers:
+nums=[8, 5, 4, 7, 2]
+n=len(nums)
+li=[]
+for i in range(n):
+    if nums[i]%2==0 :
+        li.append(nums[i])
+m=len(li)
+for i in range(m-1):
+    for j in range(m-i-1):
+        if li[j]>li[j+1]:
+            li[j],li[j+1]=li[j+1],li[j]
+a=0
+for k in range(n):
+    if nums[k]%2==0:
+        nums[k]=li[a]
+        a+=1
+print(nums)
+#contains duplicates:
+nums=[1,2,3,1]
+n=len(nums)
+for i in range(n-1):
+    swap=False
+    for j in range(n-i-1):
+        if nums[j]>nums[j+1]:
+            nums[j],nums[j+1]=nums[j+1],nums[j]
+            swap=True
+    if not swap:
+        break
+found=False
+for k in range(len(nums)-1):
+    if nums[k]==nums[k+1]:
+        found=True
+        break
+print(found)
