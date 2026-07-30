2b939c6916d81714263eaba27c6089c41deb0a6e
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-07-30 18:25:02 +0530

    added programs

--- insertion_sort/problems.py
+++ insertion_sort/problems.py
@@ -47,3 +47,28 @@ for i in range(1,n):
         j-=1
     nums[j+1]=key
 print(nums)
+#sorting in descending order:
+nums=[64, 34, 25, 12, 22, 11, 90, 5]
+n=len(nums)
+for i in range(1,n):
+    key=nums[i]
+    j=i-1
+    while j>=0 and nums[j]<key:
+        nums[j+1]=nums[j]
+        j-=1
+    nums[j+1]=key
+print(nums)
+#count the number of shifts:
+nums=[64, 34, 25, 12, 22, 11, 90, 5]
+n=len(nums)
+shifts=0
+for i in range(1,n):
+    key=nums[i]
+    j=i-1
+    while j>=0 and nums[j]>key:
+        nums[j+1]=nums[j]
+        shifts+=1
+        j-=1
+    nums[j+1]=key
+print(nums)
+print(shifts)
