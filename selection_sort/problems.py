c219cf6f5b5fb2af97193ec6a64c641a9ca62b9a
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-07-31 21:00:55 +0530

    added a program of selection sort

---
+++ selection_sort/problems.py
@@ -0,0 +1,24 @@
+#basic selection sort implement :
+nums=[64, 34, 25, 12, 22, 11, 90, 5]
+n=len(nums)
+for i in range(n-1):
+    min_idx=i
+    for j in range(i+1,n):
+        if nums[j]<nums[min_idx]:
+            min_idx=j
+    nums[i],nums[min_idx]=nums[min_idx],nums[i]
+print(nums)
+#finding the number of passes :
+nums=[64, 34, 25, 12, 22, 11, 90, 5]
+n=len(nums)
+max_pass=0
+for i in range(n-1):
+    min_idx=i
+    max_pass=1
+    for j in range(i+1,n):
+        if nums[j]<nums[min_idx]:
+            min_idx=j
+    nums[i],nums[min_idx]=nums[min_idx],nums[i]
+    max_pass+=1
+print(max_pass)
+'''time complexity of selection sort : o(n**2)'''
