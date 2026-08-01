407087da42242e26346ff763816ef73c98c5ff8d
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-08-01 21:22:48 +0530

    added a problem

--- selection_sort/problems.py
+++ selection_sort/problems.py
@@ -22,3 +22,23 @@ for i in range(n-1):
     max_pass+=1
 print(max_pass)
 '''time complexity of selection sort : o(n**2)'''
+#sorting an array in descending order:
+nums=[5, 1, 4, 2, 8]
+n=len(nums)
+for i in range(n-1):
+    min_idx=i
+    for j in range(i+1,n):
+        if nums[j]>nums[min_idx]:
+            min_idx=j
+    nums[i],nums[min_idx]=nums[min_idx],nums[i]
+print(nums)
+#sorting an array containing duplicates:
+nums=[4, 2, 4, 1, 2]
+n=len(nums)
+for i in range(n-1):
+    min_idx=i
+    for j in range(i+1,n):
+        if nums[j]<nums[min_idx]:
+            min_idx=j
+    nums[i],nums[min_idx]=nums[min_idx],nums[i]
+print(nums)
