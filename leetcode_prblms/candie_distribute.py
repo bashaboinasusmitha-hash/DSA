77fe6b89d687c54f90dd302432afce13a90e7f0a
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-08-04 21:37:51 +0530

    added a program distribution of candies to people

--- leetcode_prblms/candie_distribute.py
+++ leetcode_prblms/candie_distribute.py
@@ -15,3 +15,17 @@ s=set(candyType)
 m=len(candyType)
 n=len(s)
 print(min(m,n))
+#distribution of candies to people:
+candies = 7
+num_people = 4
+li=[0]*num_people
+give=1
+while candies>0:
+    for i in range(num_people):
+        if candies==0:
+            break
+        total=min(give,candies)
+        li[i]+=total
+        candies-=total
+        give+=1
+print(li)
