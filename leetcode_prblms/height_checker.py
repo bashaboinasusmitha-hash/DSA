09f099ec097e4a0aedbaf24b2e6aa99b53ff58df
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-08-10 12:46:42 +0530

    added a problem of relative sort array

--- leetcode_prblms/height_checker.py
+++ leetcode_prblms/height_checker.py
@@ -15,3 +15,22 @@ for m in range(n):
     if heights[m]!=b[m]:
         ans+=1
 print(ans)
+#relative sort array:
+arr1 = [2,3,1,3,2,4,6,7,9,2,19] 
+arr2=[2,1,4,3,9,6]
+n=len(arr1)
+key=max(arr1)
+count=[0]*(key+1)
+ans=[]
+for i in range(n):
+    count[arr1[i]]+=1
+for j in range(len(arr2)):
+    while count[arr2[j]]>0:
+        ans.append(arr2[j])
+        count[arr2[j]]-=1
+print(ans)
+for k in range(len(count)):
+    while count[k]>0:
+        ans.append(k)
+        count[k]-=1
+print(ans)
