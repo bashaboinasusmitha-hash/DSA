9c56bc34d0d75d182b5bdc81b79451df75a7b79b
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-08-12 19:54:03 +0530

    added a program of sort by parity and II

--- merge_sort/merge_practice.py
+++ merge_sort/merge_practice.py
@@ -32,3 +32,38 @@ nums= [15, 5, 24, 8, 1, 3, 16, 10, 20]
 merge_sort(nums,0,len(nums)-1)
 print(nums)
 
+#sort by parity:
+nums=[1,2,3,4]
+n=len(nums)
+li=[]
+li_1=[]
+for i in range(n):
+    if nums[i]%2==0:
+        li.append(nums[i])
+    elif nums[i]%2!=0:
+        li_1.append(nums[i])
+final=li+li_1
+print(final)
+
+#sort an array by parity II
+nums=[4,2,6,5,7,9]
+n=len(nums)
+even=[]
+odd=[]
+for i in range(n):
+    if nums[i]%2==0:
+        even.append(nums[i])
+    else:
+        odd.append(nums[i])
+ans=[0]*len(nums)
+e=0
+o=0
+final=even+odd
+for j in range(len(final)):
+    if j%2==0:
+        ans[j]=even[e]
+        e+=1
+    else:
+        ans[j]=odd[o]
+        o+=1
+print(ans)
