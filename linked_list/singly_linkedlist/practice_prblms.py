dcd17986e5293c075cc0838398d528f317763a31
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-08-19 23:59:16 +0530

    added A PROGRAM

---
+++ linked_list/singly_linkedlist/practice_prblms.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self,val):
+        self.val=val
+        self.next=None
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(6)
+head.next.next.next = Node(3)
+head.next.next.next.next = Node(4)
+head.next.next.next.next.next = Node(5)
+head.next.next.next.next.next.next = Node(6)
+val=6
+while head and head.val==val:
+    head=head.next
+temp=head
+while temp and temp.next:
+    if temp.next.val==val:
+        temp.next=temp.next.next
+    else:
+        temp=temp.next
+temp=head
+while temp:
+    print(temp.val,end="->")
+    temp=temp.next
+print("NULL")#1->2->3->4->5->NULL
+
