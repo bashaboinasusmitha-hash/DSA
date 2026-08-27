dcb14617f47d10082a047539f826fea49b394ec4
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-08-27 20:45:39 +0530

    added a program

--- linked_list/singly_linkedlist/practice_prblms.py
+++ linked_list/singly_linkedlist/practice_prblms.py
@@ -164,3 +164,18 @@ while temp:
     temp=temp.next
 print("NULL")#10->20->40->NULL
 
+#counting the number of nodes:
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+head=Node(10)
+head.next=Node(20)
+head.next.next=Node(40)
+head.next.next.next=Node(30)
+temp=head
+ans=0
+while temp:
+    ans+=1
+    temp=temp.next
+print(ans)#4
